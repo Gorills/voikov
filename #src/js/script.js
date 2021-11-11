6a25bc592572b2ca4bdc854be820d4c3b2486732
Gorills <gorivanickiy@gmail.com>
2021-11-11 14:45:39 +0700

    fix js scroll

--- #src/js/script.js
+++ #src/js/script.js
@@ -73,121 +73,129 @@ $('.popup-call__closer, .popup-call__close').click(function(e){
 
 });
 
-$(document).ready(function(){
-  var heights = [];  
-
-  $(".block").each(function(indx, element){
-    heights.push($(element).height());
+if(window.location.pathname=='/'){
+  $(document).ready(function(){
+    var heights = [];  
+  
+    $(".block").each(function(indx, element){
+      heights.push($(element).height());
+      
+    });
     
-  });
-  
-  var a = heights; 
-  var result=0;
-  $.each(a, function(ind,val){
-    result+=val;
-  });
-  
-
-  
-
-  $(window).scroll(function(){
-      
-      var delayTop = $('#block1').offset().top;
-      
-      var scrolls = $(window).scrollTop();
-      
-      var start = scrolls - delayTop + 125;
-      var stop = result + delayTop;
-      
+    var a = heights; 
+    var result=0;
+    $.each(a, function(ind,val){
+      result+=val;
+    });
     
-      
-      if(start >= 0 && scrolls < stop){
-          
-          $('.sidebar').css({
-            "padding-top": start + "px",
-          })
-
-      } 
-
-
-      
-  })
-})
-
-
-
-$(window).scroll(function(){
-  var blockOne = $('#block1').offset().top
-  var heightOne = $('#block1').height()
-  var topBlockOne = heightOne + blockOne
-
-  var blockTwo = $('#block2').offset().top
-  var heighTwo = $('#block2').height()
-  var topBlockTwo = blockTwo + heighTwo
-
-  var blockTree = $('#block3').offset().top
-  var heighTree = $('#block3').height()
-  var topBlockTree = blockTree + heighTree
-
-  var blockFor = $('#block4').offset().top
-  var heighFor = $('#block4').height()
-  var topBlockFor = blockFor + heighFor
-
-  var blockFive = $('#block5').offset().top
-  var heighFive = $('#block5').height()
-  var topBlockFive = blockFive + heighFive
-
-  var blockSix = $('#block6').offset().top
-  var heighSix = $('#block6').height()
-  var topBlockSix = blockSix + heighSix
-
-  var topScr = $(window).scrollTop() + 100;
   
-  if (topScr >= blockOne && topScr <= topBlockOne) {
-    $('#link1').addClass('sidebar__link--active')
-
-  } else {
-    $('#link1').removeClass('sidebar__link--active')
-  }
-
-  if (topScr >= blockTwo && topScr <= topBlockTwo) {
-    $('#link2').addClass('sidebar__link--active')
-
-  } else {
-    $('#link2').removeClass('sidebar__link--active')
-  }
-
-  if (topScr >= blockTree && topScr <= topBlockTree) {
-    $('#link3').addClass('sidebar__link--active')
-
-  } else {
-    $('#link3').removeClass('sidebar__link--active')
-  }
-
-  if (topScr >= blockFor && topScr <= topBlockFor) {
-    $('#link4').addClass('sidebar__link--active')
-
-  } else {
-    $('#link4').removeClass('sidebar__link--active')
-  }
-
-  if (topScr >= blockFive && topScr <= topBlockFive) {
-    $('#link5').addClass('sidebar__link--active')
-
-  } else {
-    $('#link5').removeClass('sidebar__link--active')
-  }
-
-  if (topScr >= blockSix && topScr <= topBlockSix) {
-    $('#link6').addClass('sidebar__link--active')
-
-  } else {
-    $('#link6').removeClass('sidebar__link--active')
-  }
+    
   
+    $(window).scroll(function(){
+        
+        var delayTop = $('#block1').offset().top;
+        
+        var scrolls = $(window).scrollTop();
+        
+        var start = scrolls - delayTop + 125;
+        var stop = result + delayTop;
 
         
-})
+        if(start >= 0 && scrolls < stop){
+            
+            $('.sidebar').css({
+              "padding-top": start + "px",
+            })
+  
+        } 
+  
+  
+        
+    })
+  })
+  
+  
+  
+  $(window).scroll(function(){
+    var blockOne = $('#block1').offset().top
+    var heightOne = $('#block1').height()
+    var topBlockOne = heightOne + blockOne
+  
+    var blockTwo = $('#block2').offset().top
+    var heighTwo = $('#block2').height()
+    var topBlockTwo = blockTwo + heighTwo
+  
+    var blockTree = $('#block3').offset().top
+    var heighTree = $('#block3').height()
+    var topBlockTree = blockTree + heighTree
+  
+    var blockFor = $('#block4').offset().top
+    var heighFor = $('#block4').height()
+    var topBlockFor = blockFor + heighFor
+  
+    var blockFive = $('#block5').offset().top
+    var heighFive = $('#block5').height()
+    var topBlockFive = blockFive + heighFive
+  
+    var blockSix = $('#block6').offset().top
+    var heighSix = $('#block6').height()
+    var topBlockSix = blockSix + heighSix
+  
+    var topScr = $(window).scrollTop() + 150;
+    
+    if (topScr >= blockOne && topScr <= topBlockOne) {
+      $('#link1').addClass('sidebar__link--active')
+  
+    } else {
+      $('#link1').removeClass('sidebar__link--active')
+    }
+  
+    if (topScr >= blockTwo && topScr <= topBlockTwo) {
+      $('#link2').addClass('sidebar__link--active')
+  
+    } else {
+      $('#link2').removeClass('sidebar__link--active')
+    }
+  
+    if (topScr >= blockTree && topScr <= topBlockTree) {
+      $('#link3').addClass('sidebar__link--active')
+  
+    } else {
+      $('#link3').removeClass('sidebar__link--active')
+    }
+  
+    if (topScr >= blockFor && topScr <= topBlockFor) {
+      $('#link4').addClass('sidebar__link--active')
+  
+    } else {
+      $('#link4').removeClass('sidebar__link--active')
+    }
+  
+    if (topScr >= blockFive && topScr <= topBlockFive) {
+      $('#link5').addClass('sidebar__link--active')
+  
+    } else {
+      $('#link5').removeClass('sidebar__link--active')
+    }
+  
+    if (topScr >= blockSix && topScr <= topBlockSix) {
+      $('#link6').addClass('sidebar__link--active')
+  
+    } else {
+      $('#link6').removeClass('sidebar__link--active')
+    }
+    
+  
+          
+  })
+  
+
+
+ } else {
+  
+ }
+
+
 
 
 
